aba9b14772ff1d3bad24b255fe25bce90126bbc1
birsnot <birsnot@gmail.com>
2025-05-24 07:56:34 -0300

    added batch processing

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,28 @@
+import seed
+
+def stream_users_in_batches(batch_size):
+    connection = seed.connect_to_prodev()
+    if not connection:
+        return
+
+    cursor = connection.cursor(dictionary=True)
+    offset = 0
+
+    while True:
+        cursor.execute(
+            "SELECT * FROM user_data LIMIT %s OFFSET %s", (batch_size, offset)
+        )
+        rows = cursor.fetchall()
+        if not rows:
+            break
+        yield rows
+        offset += batch_size
+
+    cursor.close()
+    connection.close()
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user["age"] > 25:
+                print(user)
