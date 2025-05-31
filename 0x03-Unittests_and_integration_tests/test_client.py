93779de28809b8f633918929d5036f14fb1343c7
birsnot <birsnot@gmail.com>
2025-05-31 05:18:13 -0300

    added test_has_license

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -57,3 +57,12 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(result, ["repo1", "repo2", "repo3"])
             mock_get_json.assert_called_once()
             mock_repos_url.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test that has_license returns expected value"""
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
