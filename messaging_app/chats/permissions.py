20a9442c7cfd61514e6c54ff135b3028b93bb1d7
birsnot <birsnot@gmail.com>
2025-06-08 08:41:43 -0300

    fixed permissions.py

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,24 +1,22 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
-from .models import Conversation, Message
+from rest_framework import permissions
+from .models import Message
 
-class IsParticipantOfConversation(BasePermission):
+class IsParticipantOfConversation(permissions.BasePermission):
     """
-    Allows access only to participants of a conversation.
+    - Allows only authenticated users
+    - Allows only participants to interact with conversation messages
+    - Allows only the sender to edit/delete messages
     """
 
     def has_permission(self, request, view):
-        # Authenticated users only
         return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        user = request.user
-
-        # If the object is a Message, check if user is part of its conversation
         if isinstance(obj, Message):
-            return user in obj.conversation.participants.all()
-
-        # If the object is a Conversation, check directly
-        if isinstance(obj, Conversation):
-            return user in obj.participants.all()
-
+            # All participants can view or send messages
+            if request.method in permissions.SAFE_METHODS or request.method == "POST":
+                return request.user in obj.conversation.participants.all()
+            # Only sender can update/delete
+            elif request.method in ["PUT", "PATCH", "DELETE"]:
+                return obj.sender == request.user
         return False
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,17 +1,17 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+from rest_framework import status
+
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 from .permissions import IsParticipantOfConversation
 
-
 class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
-
-    def get_queryset(self):
-        # Only conversations where the user is a participant
-        return Conversation.objects.filter(participants=self.request.user)
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         conversation = serializer.save()
@@ -20,11 +20,25 @@ class ConversationViewSet(viewsets.ModelViewSet):
 
 class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
+    permission_classes = [IsParticipantOfConversation]
 
     def get_queryset(self):
-        # Only messages in conversations where the user is a participant
-        return Message.objects.filter(conversation__participants=self.request.user)
+        conversation_id = self.kwargs.get('conversation_pk')
+        try:
+            conversation = Conversation.objects.get(conversation_id=conversation_id)
+        except Conversation.DoesNotExist:
+            return Message.objects.none()
+
+        if self.request.user not in conversation.participants.all():
+            raise PermissionDenied(detail="You are not a participant of this conversation", code=status.HTTP_403_FORBIDDEN)
+
+        return Message.objects.filter(conversation=conversation)
 
     def perform_create(self, serializer):
-        serializer.save(sender=self.request.user)
+        conversation_id = self.kwargs.get('conversation_pk')
+        conversation = Conversation.objects.get(conversation_id=conversation_id)
+
+        if self.request.user not in conversation.participants.all():
+            raise PermissionDenied(detail="You are not a participant of this conversation", code=status.HTTP_403_FORBIDDEN)
+
+        serializer.save(sender=self.request.user, conversation=conversation)
